b955d38d66d731273d1a7ecc39c2516585d7fdbb
Ranganath <ransnbvr@gmail.com>
2025-08-20 19:55:30 +0530

    setting nltk data path

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from models.model import recommend_products, top5_products
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
-nltk.data.path.append(os.path.join(BASE_DIR, "..", "nltk_data"))
+nltk.data.path.append(os.path.join(BASE_DIR, "nltk_data"))
 
 valid_userid = ['00sab00','1234','zippy','zburt5','joshua','dorothy w','rebecca','walker557','samantha','raeanne','kimmie','cassie','moore222']
 
